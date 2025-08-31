0f759914f9cf62f3bc5ac443101822f5472107fb
Dev Hamid <abdulhamidolash1@gmail.com>
2025-08-31 14:13:33 +0100

    onclick rotation

--- design/index.js
+++ design/index.js
@@ -2,12 +2,15 @@ const features = document.querySelector('#features');
 const dropdown = document.querySelector('.dropdown-content');
 const company = document.querySelector('#company')
 const dropdownConst = document.querySelector('.company')
+const featuresArrowDown = features.querySelector('img')
+
 
 
 features.addEventListener('click', () => {
-    dropdown.classList.toggle('dropdown-display')  
+    dropdown.classList.toggle('dropdown-display')
+    featuresArrowDown.classList.toggle('rotate-img')
+    
 });
 company.addEventListener('click', () => {
     dropdownConst.classList.toggle('dropdown-display')  
-    console.log('cliched')
 });
